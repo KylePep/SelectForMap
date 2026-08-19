5f8ea94e4a58f2f8f915dc29e4a9b4b5d94bb489
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-08-19 17:53:50 -0600

    dropped login/register view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
 import HomeView from "../views/HomeView.vue"
 import MapView from '../views/MapView.vue'
 import { useAuthStore } from '../stores/auth'
@@ -9,8 +7,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/home', component: HomeView },
-    { path: '/login', component: LoginView },
-    { path: '/register', component: RegisterView },
     { path: '/map', component: MapView, meta: { requiresAuth: true } },
     { path: '/', redirect: '/home' },
   ],
@@ -19,7 +15,7 @@ const router = createRouter({
 router.beforeEach((to) => {
   const auth = useAuthStore()
   if (to.meta.requiresAuth && !auth.isAuthenticated) {
-    return '/login'
+    return '/home'
   }
 })
 
